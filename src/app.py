579316d5e2f58be636e947ddd7ce101e621eddf9
Feng <2678045931@qq.com>
2026-03-08 12:27:44 +0800

    Change Uvicorn host to 0.0.0.0

--- src/app.py
+++ src/app.py
@@ -1013,4 +1013,4 @@ def api_stats(db: Session = Depends(get_db)):
 
 
 if __name__ == "__main__":
-	uvicorn.run("src.app:app", host="127.0.0.1", port=8000, reload=True)
+	uvicorn.run("src.app:app", host="0.0.0.0", port=8000, reload=True)
