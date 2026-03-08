6853636d31651cbcff9c0501b925dcaaf6cb8e0e
Feng <2678045931@qq.com>
2026-03-08 14:16:25 +0800

    Update run.py

--- run.py
+++ run.py
@@ -7,4 +7,4 @@ from src.app import app
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("src.app:app", host="0.0.0.0", port=8000)
+    uvicorn.run("src.app:app", host="0.0.0.0", port=5000)
